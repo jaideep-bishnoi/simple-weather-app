8d0110bcf6a2ff800f0daf5bf7d46e1981e639f7
Jaideep Bishnoi <jaideepbishnoi@gmail.com>
2016-12-17 00:16:06 +0530

    Update weather-item.component.ts

--- app/ts/weather/weather-item.component.ts
+++ app/ts/weather/weather-item.component.ts
@@ -3,7 +3,7 @@ import {WeatherItem} from './weather-item';
 
 @Component({
 	selector: 'weather-item',
-	template: '<article class="col-lg-12 text-center v-center weather-element"><div class="col-1"><h3>{{ weatherItem.cityName }}</h3><p class="weather-type">{{ weatherItem.description }}</p></div><div class="col-2"><span class="temperature">{{ weatherItem.temperature }}° C</span></div></article>',
+	template: '<article class="col-lg-12 text-center v-center weather-element"><div class="col-md-8"><h3>{{ weatherItem.cityName }}</h3><p class="weather-type">{{ weatherItem.description }}</p></div><div class="col-md-4"><span class="temperature">{{ weatherItem.temperature }}° C</span></div></article>',
 	styleUrls: ['app/css/weather-item.css'],
 	inputs: ['weatherItem:item'],
 })
